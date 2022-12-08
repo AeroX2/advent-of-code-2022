db586609d490ed5a9f44f2cf9c07e0a6ad4eed17
James <james.r@canva.com>
2022-12-08 20:36:36 +1100

    ch8

--- src/main/kotlin/ch8/Main.kt
+++ src/main/kotlin/ch8/Main.kt
@@ -3,7 +3,7 @@ package ch8
 data class Tree(val x: Int, val y: Int, val height: Int)
 
 fun main() {
-    val data = object {}.javaClass.getResource("/ch8/example.txt")?.readText()
+    val data = object {}.javaClass.getResource("/ch8/input.txt")?.readText()
     if (data == null) {
         println("Missing data file")
         return
@@ -15,9 +15,15 @@ fun main() {
 }
 
 fun visible(lines: List<List<Tree>>): List<List<Tree>> {
-    val max = lines.map { it.withIndex().windowed(2).indexOfFirst { it[1].value.height < it[0].value.height } }
-    println(max)
-    return lines.mapIndexed { index, chars -> chars.subList(0, if (max[index] != -1) max[index]+1 else 1) }
+    return lines.map { line ->
+        line.fold(Pair(-1, emptyList<Tree>())) { acc, tree ->
+            if (tree.height > acc.first) {
+                Pair(tree.height, acc.second + tree)
+            } else {
+                acc
+            }
+        }.second
+    }
 }
 
 fun transpose(lines: List<List<Tree>>): List<List<Tree>> {
@@ -29,31 +35,41 @@ fun part1(lines: List<List<Tree>>): Int {
     val rightSide = visible(lines.map { it.reversed() })
     val topSide = visible(transpose(lines))
     val bottomSide = visible(transpose(lines).map { it.reversed() })
-    println("L")
-    println(leftSide.map { it.map { it.height } })
-    println("R")
-    println(rightSide.map { it.map { it.height } })
-    println("B")
-    println(topSide.map { it.map { it.height } })
-    println("T")
-    println(bottomSide.map { it.map { it.height } })
-    println()
 
-    val g = (leftSide + rightSide + topSide + bottomSide).flatten().toSet()
-    println(g)
-    println(g.size)
+    val visibleTrees = (leftSide + rightSide + topSide + bottomSide).flatten().toSet()
 
-    for (y in 0..10) {
-        for (x in 0..10) {
-            val f = g.any { it.x == x && it.y == y }
-            print(if (f) '#' else ' ')
-        }
-        println()
-    }
+//    for (y in 0..lines.size) {
+//        for (x in 0..lines[0].size) {
+//            val f = visibleTrees.any { it.x == x && it.y == y }
+//            print(if (f) lines[y][x].height else ' ')
+//        }
+//        println()
+//    }
 
-    return 0
+    return visibleTrees.size
+}
+
+fun visible2(lines: List<List<Tree>>, x: Int, y: Int): Int {
+    val tree = lines[y][x]
+    val treeLine = lines[y].take(x).reversed()
+    val i = treeLine.indexOfFirst { it.height >= tree.height }
+    return if (i == -1) treeLine.size else i+1
 }
 
 fun part2(lines: List<List<Tree>>): Int {
-    return 0
+    val leftSide = lines
+    val rightSide = lines.map { it.reversed() }
+    val topSide = transpose(lines)
+    val bottomSide = transpose(lines).map { it.reversed() }
+
+    return lines.mapIndexed { y, line ->
+        List(line.size) { x ->
+            val v1 = visible2(leftSide, x, y)
+            val v2 = visible2(rightSide, line.size - x - 1, y)
+            val v3 = visible2(topSide, y, x)
+            val v4 = visible2(bottomSide, lines.size - y - 1, x)
+
+            v1 * v2 * v3 * v4
+        }.maxOf { it }
+    }.maxOf { it }
 }
