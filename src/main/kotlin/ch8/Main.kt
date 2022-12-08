cbefd4bc81426d1f19275645d8c2c0c1281e34ef
James <james.r@canva.com>
2022-12-08 18:56:22 +1100

    ch8 wip

---
+++ src/main/kotlin/ch8/Main.kt
@@ -0,0 +1,59 @@
+package ch8
+
+data class Tree(val x: Int, val y: Int, val height: Int)
+
+fun main() {
+    val data = object {}.javaClass.getResource("/ch8/example.txt")?.readText()
+    if (data == null) {
+        println("Missing data file")
+        return
+    }
+
+    val lines = data.split("\n").mapIndexed { y, l -> l.mapIndexed { x, h -> Tree(x, y, h.digitToInt()) } }
+    println(part1(lines))
+    println(part2(lines))
+}
+
+fun visible(lines: List<List<Tree>>): List<List<Tree>> {
+    val max = lines.map { it.withIndex().windowed(2).indexOfFirst { it[1].value.height < it[0].value.height } }
+    println(max)
+    return lines.mapIndexed { index, chars -> chars.subList(0, if (max[index] != -1) max[index]+1 else 1) }
+}
+
+fun transpose(lines: List<List<Tree>>): List<List<Tree>> {
+    return List(lines[0].size) { colIndex -> lines.map { it[colIndex] } }
+}
+
+fun part1(lines: List<List<Tree>>): Int {
+    val leftSide = visible(lines)
+    val rightSide = visible(lines.map { it.reversed() })
+    val topSide = visible(transpose(lines))
+    val bottomSide = visible(transpose(lines).map { it.reversed() })
+    println("L")
+    println(leftSide.map { it.map { it.height } })
+    println("R")
+    println(rightSide.map { it.map { it.height } })
+    println("B")
+    println(topSide.map { it.map { it.height } })
+    println("T")
+    println(bottomSide.map { it.map { it.height } })
+    println()
+
+    val g = (leftSide + rightSide + topSide + bottomSide).flatten().toSet()
+    println(g)
+    println(g.size)
+
+    for (y in 0..10) {
+        for (x in 0..10) {
+            val f = g.any { it.x == x && it.y == y }
+            print(if (f) '#' else ' ')
+        }
+        println()
+    }
+
+    return 0
+}
+
+fun part2(lines: List<List<Tree>>): Int {
+    return 0
+}
